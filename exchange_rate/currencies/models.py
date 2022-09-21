c7502351eb579d89a24f77c46f86374b76a4ba56
john <izolotavin99@gmail.com>
2022-09-21 15:29:32 +0300

    1) currencies.models.Currency added 2) 0001_initial migration migrated

--- exchange_rate/currencies/models.py
+++ exchange_rate/currencies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    """Модель Валюты"""
+
+    name = models.CharField('название', max_length=32, unique=True)
+    rate = models.FloatField('курс к рублю')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'валюта'
+        verbose_name_plural = 'валюты'
