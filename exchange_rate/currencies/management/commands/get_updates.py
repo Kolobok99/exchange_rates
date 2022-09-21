0092ed74ca69409319b7d4bb4c0d403ecc0edd25
john <izolotavin99@gmail.com>
2022-09-21 15:56:29 +0300

    1) currencies namage.py get_updates command added 2) services.services.get_exchange_rates_from_cbr_ru added

---
+++ exchange_rate/currencies/management/commands/get_updates.py
@@ -0,0 +1,21 @@
+from django.core.management import BaseCommand
+
+from currencies.models import Currency
+from services.services import get_exchange_rates_from_cbr_ru
+
+
+# from currencies.models import Currency
+# from services.services import get_exchange_rates_from_cbr_ru
+
+
+class Command(BaseCommand):
+    help = 'Обновление/Добавление Currency.rate'
+
+    def handle(self, *args, **options):
+        valutes = get_exchange_rates_from_cbr_ru()
+
+        for valute in valutes:
+            Currency.objects.update_or_create(
+                name=valute[0],
+                rate=valute[1],
+                defaults={'name': valute[0]})
