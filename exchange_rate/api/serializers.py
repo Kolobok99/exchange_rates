8cacdf0f4533e445f5b3eaef6d2a2a7b0bf202f0
john <izolotavin99@gmail.com>
2022-09-21 20:03:03 +0300

    1) test.tests_api.tests_serializer.CurrencySerializerTestCase added 2) CurrencySerializerTestCase.test_contains_expected_fields() added 3) CurrencySerializerTestCase.test_name_field_content() added 4) CurrencySerializerTestCase.test_rate_field_content() added 5) CurrencySerializerTestCase.test_creating_instance_is_forbidden() added 6) CurrencySerializerTestCase.test_updating_instance_is_forbidden() added

--- exchange_rate/api/serializers.py
+++ exchange_rate/api/serializers.py
@@ -1,11 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from currencies.models import Currency
 
 
-class CurrencySerializer(ModelSerializer):
+# class ReadOnlyModelSerializer(serializers.ModelSerializer):
+#     def get_fields(self, *args, **kwargs):
+#         fields = super().get_fields(*args, **kwargs)
+#         for field in fields:
+#             fields[field].read_only = True
+#         return fields
+
+class CurrencySerializer(serializers.ModelSerializer):
     """Сериализатор модели Currency"""
 
     class Meta:
         model = Currency
         fields = "__all__"
+        read_only_fields = [f.name for f in Currency._meta.get_fields()]
---
+++ exchange_rate/tests/tests_api/tests_serializers.py
@@ -0,0 +1,60 @@
+from decimal import Decimal
+
+from django.db import IntegrityError
+from django.test import TestCase
+
+from api.serializers import CurrencySerializer
+from currencies.models import Currency
+
+
+class CurrencySerializerTestCase(TestCase):
+    """Класс тестирования CurrencySerializer"""
+
+    def setUp(self):
+        self.currency_attributes = {
+            'name': 'USD',
+            'rate': 60.12
+        }
+
+        self.serializer_data = {
+            'name': 'EURO',
+            'rate': 68.12
+        }
+
+        self.currency = Currency.objects.create(**self.currency_attributes)
+        self.serializer = CurrencySerializer(instance=self.currency)
+
+    def test_contains_expected_fields(self):
+        """Тест: сериализатор содержит нужные атрибуты"""
+        data = self.serializer.data
+        self.assertEqual(data.keys(), {'id', 'name', 'rate'})
+
+    def test_name_field_content(self):
+        """Тест: сериализатор содежрит ожидаемое значение для 'name'"""
+        data = self.serializer.data
+
+        self.assertEqual(data['name'], self.currency_attributes['name'])
+
+    def test_rate_field_content(self):
+        """Тест: сериализатор содежрит ожидаемое значение для 'rate'"""
+        data = self.serializer.data
+
+        self.assertEqual(data['rate'], self.currency_attributes['rate'])
+
+    def test_creating_instance_is_forbidden(self):
+        """Тест: создание записей запрещено"""
+
+        serializer = CurrencySerializer(data=self.serializer_data)
+        serializer.is_valid()
+        self.assertNotEqual(0, len(self.serializer_data))
+        self.assertEqual(0, len(serializer.data))
+
+    def test_updating_instance_is_forbidden(self):
+        """Тест: обновление записи возвращает туже запись"""
+        instance = Currency.objects.first()
+        serializer = CurrencySerializer(instance=instance, data=self.serializer_data)
+        serializer.is_valid()
+        serializer.save()
+
+        self.assertEqual(serializer.data['name'], instance.name)
+        self.assertEqual(serializer.data['rate'], instance.rate)
