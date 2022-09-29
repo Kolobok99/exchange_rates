88db4254f0f63a0dac7e897f39e1d167130a46fc
john <izolotavin99@gmail.com>
2022-09-29 16:18:14 +0300

    1) .gitlab-ci.yml new

--- exchange_rates/core/settings.py
+++ exchange_rates/core/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'ENGINE': os.environ.get('SQL_ENGINE', default='django.db.backends.mysql'),
         'NAME': os.environ.get('SQL_NAME', default='db'),
         'USER': os.environ.get('SQL_USER', default='root'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', default='rootroot'),
+        'PASSWORD': os.environ.get('SQL_ROOT_PASSWORD', default='12345'),
         'HOST': os.environ.get('SQL_HOST', default='localhost'),
         'PORT': os.environ.get('SQL_PORT', default='3306'),
     }
