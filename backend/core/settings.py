71662f0c3403bac7a7806011c09950c293d62804
john <izolotavin99@gmail.com>
2022-10-03 19:41:48 +0300

    ci ci ci . . .

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('SQL_ENGINE', default='django.db.backends.mysql'),
-        'NAME': os.environ.get('SQL_NAME', default='db'),
+        'NAME': os.environ.get('SQL_DATABASE', default='db'),
         'USER': os.environ.get('SQL_USER', default='root'),
         'PASSWORD': os.environ.get('QL_ROOT_PASSWORD', default='12345'),
         'HOST': os.environ.get('SQL_HOST', default='db'),
