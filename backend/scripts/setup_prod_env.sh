f6828b60bab13fb15a3e2a2cfdd33cf82b37e49c
john <izolotavin99@gmail.com>
2022-10-04 12:36:22 +0300

    ci ci ci . . .

--- backend/scripts/setup_prod_env.sh
+++ backend/scripts/setup_prod_env.sh
@@ -9,21 +9,21 @@ mkdir ./backend/.env_files/.env.prod/
 
 echo DEBUG=0 >> ./backend/.env_files/.env.prod/.env.prod.settings
 
-#echo SECRET_KEY=$SECRET_KEY >> .env.prod.settings
-#echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] >> .env.prod.settings
+echo SECRET_KEY=$SECRET_KEY >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] >> ./backend/.env_files/.env.prod/.env.prod.settings
 #
-#echo SQL_ENGINE=$SQL_ENGINE >> .env.prod.settings
-#echo SQL_NAME=$SQL_NAME >> .env.prod.settings
-#echo SQL_USER=$SQL_USER >> .env.prod.settings
-#echo SQL_PASSWORD=$SQL_PASSWORD >> .env.prod.settings
-#echo SQL_HOST=$SQL_HOST >> .env.prod.settings
-#echo SQL_PORT=$SQL_PORT >> .env.prod.settings
+echo SQL_ENGINE=$SQL_ENGINE >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_NAME=$SQL_NAME >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_USER=$SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_PASSWORD=$SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_HOST=$SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_PORT=$SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.settings
 #
-#echo MYSQL_DATABASE=$SQL_HOST >> .env.prod.db
-#echo MYSQL_USER=$SQL_USER >> .env.prod.db
-#echo MYSQL_PORT=$SQL_PORT >> .env.prod.db
-#echo MYSQL_PASSWORD=$SQL_PASSWORD >> .env.prod.db
-#echo MYSQL_ROOT_PASSWORD=$SQL_ROOT_PASSWORD >> .env.prod.db
+echo MYSQL_DATABASE=$SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_USER=$SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_PORT=$SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_PASSWORD=$SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_ROOT_PASSWORD=$SQL_ROOT_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
 
 echo "ДОБАВИЛ!!!"
 
