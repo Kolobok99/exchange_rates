fbab33bc81ee5baa9e5b80435452e76fef6bb664
john <izolotavin99@gmail.com>
2022-10-04 12:27:58 +0300

    ci ci ci . . .

--- backend/scripts/setup_prod_env.sh
+++ backend/scripts/setup_prod_env.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
-#mkdir ./backend/.env_files/.env.prod/
+
+
+mkdir ./backend/.env_files/.env.prod/
+
+
 # shellcheck disable=SC2164
 #cd /backend/.env_files/.env.prod/
-pwd
-echo DEBUG=0 >> .env.prod.settings
-echo SECRET_KEY=$SECRET_KEY >> .env.prod.settings
-echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] >> .env.prod.settings
-
-echo SQL_ENGINE=$SQL_ENGINE >> .env.prod.settings
-echo SQL_NAME=$SQL_NAME >> .env.prod.settings
-echo SQL_USER=$SQL_USER >> .env.prod.settings
-echo SQL_PASSWORD=$SQL_PASSWORD >> .env.prod.settings
-echo SQL_HOST=$SQL_HOST >> .env.prod.settings
-echo SQL_PORT=$SQL_PORT >> .env.prod.settings
-
-echo MYSQL_DATABASE=$SQL_HOST >> .env.prod.db
-echo MYSQL_USER=$SQL_USER >> .env.prod.db
-echo MYSQL_PORT=$SQL_PORT >> .env.prod.db
-echo MYSQL_PASSWORD=$SQL_PASSWORD >> .env.prod.db
-echo MYSQL_ROOT_PASSWORD=$SQL_ROOT_PASSWORD >> .env.prod.db
+#echo DEBUG=0 >> .env.prod.settings
+#echo SECRET_KEY=$SECRET_KEY >> .env.prod.settings
+#echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] >> .env.prod.settings
+#
+#echo SQL_ENGINE=$SQL_ENGINE >> .env.prod.settings
+#echo SQL_NAME=$SQL_NAME >> .env.prod.settings
+#echo SQL_USER=$SQL_USER >> .env.prod.settings
+#echo SQL_PASSWORD=$SQL_PASSWORD >> .env.prod.settings
+#echo SQL_HOST=$SQL_HOST >> .env.prod.settings
+#echo SQL_PORT=$SQL_PORT >> .env.prod.settings
+#
+#echo MYSQL_DATABASE=$SQL_HOST >> .env.prod.db
+#echo MYSQL_USER=$SQL_USER >> .env.prod.db
+#echo MYSQL_PORT=$SQL_PORT >> .env.prod.db
+#echo MYSQL_PASSWORD=$SQL_PASSWORD >> .env.prod.db
+#echo MYSQL_ROOT_PASSWORD=$SQL_ROOT_PASSWORD >> .env.prod.db
 
 echo "ДОБАВИЛ!!!"
 
-cd ../../..
+#cd ../../..
 
