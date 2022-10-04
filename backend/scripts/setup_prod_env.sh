6d847656e9df28e5dcd3f0e108086b08fd3e6944
john <izolotavin99@gmail.com>
2022-10-04 12:39:58 +0300

    ci ci ci . . .

--- backend/scripts/setup_prod_env.sh
+++ backend/scripts/setup_prod_env.sh
@@ -9,21 +9,21 @@ mkdir ./backend/.env_files/.env.prod/
 
 echo DEBUG=0 >> ./backend/.env_files/.env.prod/.env.prod.settings
 
-echo SECRET_KEY=$SECRET_KEY >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SECRET_KEY=$PROD_SECRET_KEY >> ./backend/.env_files/.env.prod/.env.prod.settings
 echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] >> ./backend/.env_files/.env.prod/.env.prod.settings
 #
-echo SQL_ENGINE=$SQL_ENGINE >> ./backend/.env_files/.env.prod/.env.prod.settings
-echo SQL_NAME=$SQL_NAME >> ./backend/.env_files/.env.prod/.env.prod.settings
-echo SQL_USER=$SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.settings
-echo SQL_PASSWORD=$SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.settings
-echo SQL_HOST=$SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.settings
-echo SQL_PORT=$SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_ENGINE=$PROD_SQL_ENGINE >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_NAME=$PROD_SQL_NAME >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_USER=$PROD_SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_PASSWORD=$PROD_SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_HOST=$PROD_SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.settings
+echo SQL_PORT=$PROD_SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.settings
 #
-echo MYSQL_DATABASE=$SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.db
-echo MYSQL_USER=$SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.db
-echo MYSQL_PORT=$SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.db
-echo MYSQL_PASSWORD=$SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
-echo MYSQL_ROOT_PASSWORD=$SQL_ROOT_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_DATABASE=$PROD_SQL_HOST >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_USER=$PROD_SQL_USER >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_PORT=$PROD_SQL_PORT >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_PASSWORD=$PROD_SQL_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
+echo MYSQL_ROOT_PASSWORD=$PROD_SQL_ROOT_PASSWORD >> ./backend/.env_files/.env.prod/.env.prod.db
 
 echo "ДОБАВИЛ!!!"
 
